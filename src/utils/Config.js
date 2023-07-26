927f9b5fbc754d47539bf3a573647fad4b9b18e9
seva <tarkhanov2704@mail.ru>
2023-07-26 17:24:02 +0300

    12312

--- src/utils/Config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__field',
-    submitButtonSelector: '.popup__button-submit',
-    inactiveButtonClass: 'popup__button_invalid',
-    inputErrorClass: 'popup__field_type_error',
-    errorClass: 'popup__error_visible'
-};
