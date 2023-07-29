64bc32f2a70d6b940ffa9851738fd4dd72a1b541
seva <tarkhanov2704@mail.ru>
2023-07-29 15:14:31 +0300

    1231231231

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,14 +3,30 @@ const Card = require('../models/card');
 module.exports.getCards = (_, res) => {
   Card.find({})
   .then((card) => res.send(card))
-  .catch((err) => res.status(500).send({ message: err.message }))
+  .catch((err) => {
+    if (err.name === 'CastError'){
+      res.status(400).send('Произошла ошибка: Bad Request')
+    }else{
+      res.status(500).send({
+        message: 'Произошла ошибка: Server Error'
+      })
+    }
+  })
 }
 module.exports.createCard = (req, res) => {
   cconsole.log(req.user._id);
   const {name, link} = req.body;
   Card.create({name, link})
   .then((card) => res.send(card))
-  .catch((err) => res.status(500).send({ message: err.message }))
+  .catch((err) => {
+    if (err.name === 'CastError'){
+      res.status(400).send('Произошла ошибка: Bad Request')
+    }else{
+      res.status(500).send({
+        message: 'Произошла ошибка: Server Error'
+      })
+    }
+  })
 }
 module.exports.deleteCard = (req, res, next) => {
   const { cardId } = req.params;
@@ -18,7 +34,9 @@ module.exports.deleteCard = (req, res, next) => {
   Card.findById({cardId})
   .then((card) => {
     if(!card){
-      throw new Error('Нет такой карточки')
+      return res.status(404).send({
+        message: 'Произошла ошибка: Not Found'
+      })
     }
     const ownerId = card.owner.id;
     const userId = req.user._id;
@@ -46,8 +64,23 @@ module.exports.likeCard = (req, res) => {
       new: true
     }
   )
-  .then((card) => res.send(card))
-  .catch((err) => res.status(500).send({ message: err.message }))
+  .then((card) => {
+    if(!card){
+      return res.status(404).send({
+        message: 'Произошла ошибка: Not Found'
+      })
+    }
+    res.send(card)
+  })
+  .catch((err) => {
+    if (err.name === 'CastError'){
+      res.status(400).send('Произошла ошибка: Bad Request')
+    }else{
+      res.status(500).send({
+        message: 'Произошла ошибка: Server Error'
+      })
+    }
+  })
 }
 module.exports.dislikeCard = (req, res) => {
   const { cardId } = req.params;
@@ -63,6 +96,21 @@ module.exports.dislikeCard = (req, res) => {
       new: true
     }
   )
-  .then((card) => res.send(card))
-  .catch((err) => res.status(500).send({ message: err.message }))
+  .then((card) => {
+    if(!card){
+      return res.status(404).send({
+        message: 'Произошла ошибка: Not Found'
+      })
+    }
+    res.send(card)
+  })
+  .catch((err) => {
+    if (err.name === 'CastError'){
+      res.status(400).send('Произошла ошибка: Bad Request')
+    }else{
+      res.status(500).send({
+        message: 'Произошла ошибка: Server Error'
+      })
+    }
+  })
 }
