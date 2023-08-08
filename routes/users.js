26f4212fd1004d2b4e0d8d25cab7ccfb716909c4
seva <tarkhanov2704@mail.ru>
2023-08-08 17:11:31 +0300

    3434

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsersInfo,
   getUserInfoById,
@@ -10,11 +11,37 @@ const {
 } = require('../controllers/users');
 
 router.get('/', getUsersInfo);
-router.get('/:id', getUserInfoById);
+router.get('/:id', celebrate({
+  body: Joi.object().keys({
+    id: Joi.string().length(24).hex().required(),
+  }),
+}), getUserInfoById);
 router.get('/me', getCurrentUserInfo);
-router.patch('/me', setUserInfo);
-router.patch('/me/avatar', setUserAvatar);
-router.post('/signin', loginUser);
-router.post('/signup', createUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), setUserInfo);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+  }),
+}), setUserAvatar);
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), loginUser);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+  }),
+}), createUser);
 
 module.exports = router;
