bf32958cab1c5064ede9fb420255df12b9b3da16
seva <tarkhanov2704@mail.ru>
2023-08-17 16:33:02 +0300

    23233

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new AuthError('Произошла ошибка: Auth Error 11111'));
+    return next(new AuthError('Произошла ошибка: Auth Error 11111'));
   }
 
   const token = authorization.replace('Bearer ', '');
