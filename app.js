da4b368333bac783598a3c07e2bddc3466ffe05e
seva <tarkhanov2704@mail.ru>
2023-08-17 16:07:17 +0300

    888

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use('/', routerSignUp);
 app.use('/', routerSignIn);
 
